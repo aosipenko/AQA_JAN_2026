5d613b151a4a03aac243ac3712488ebee7e4156f
Sebastian Pereyra <pereyras1976@gmail.com>
2026-02-23 22:46:15 +0200

    Homework11

--- src/main/java/org/prog/collections/session11/ListDemo.java
+++ src/main/java/org/prog/collections/session11/ListDemo.java
@@ -7,38 +7,48 @@ public class ListDemo {
 
     public static void main(String[] args) {
 
-        List<String> androids = new ArrayList<>();
 
-        androids.add("black");
-        androids.add("white");
-        androids.add("blue");
-        androids.add("green");
-        androids.add("pink");
-        androids.add("gray");
-        androids.add("red");
-        androids.add("silver");
-        androids.add("purple");
-        androids.add("orange");
+        List<Phone> androids = new ArrayList<>();
 
-        List<String> apples = new ArrayList<>();
-
-        apples.add("black");
-        apples.add("white");
-        apples.add("blue");
-        apples.add("green");
-        apples.add("gold");
-        apples.add("gray");
-        apples.add("red");
-        apples.add("silver");
-        apples.add("purple");
-        apples.add("orange");
+        androids.add(new Phone("Samsung", "black"));
+        androids.add(new Phone("Samsung", "white"));
+        androids.add(new Phone("Samsung", "blue"));
+        androids.add(new Phone("Samsung", "green"));
+        androids.add(new Phone("Samsung", "pink"));
+        androids.add(new Phone("Samsung", "gray"));
+        androids.add(new Phone("Samsung", "red"));
+        androids.add(new Phone("Samsung", "silver"));
+        androids.add(new Phone("Samsung", "purple"));
+        androids.add(new Phone("Samsung", "orange"));
 
 
-        System.out.println("Pink Android есть? " + androids.contains("pink"));
 
-        System.out.println("Gold Android есть? " + androids.contains("gold"));
+        List<Phone> apples = new ArrayList<>();
 
-        System.out.println("Pink Apple есть? " + apples.contains("pink"));
-        System.out.println("Gold Apple есть? " + apples.contains("gold"));
+        apples.add(new Phone("Apple", "black"));
+        apples.add(new Phone("Apple", "white"));
+        apples.add(new Phone("Apple", "blue"));
+        apples.add(new Phone("Apple", "green"));
+        apples.add(new Phone("Apple", "gold"));
+        apples.add(new Phone("Apple", "gray"));
+        apples.add(new Phone("Apple", "red"));
+        apples.add(new Phone("Apple", "silver"));
+        apples.add(new Phone("Apple", "purple"));
+        apples.add(new Phone("Apple", "orange"));
+
+
+
+        System.out.println("Pink Android есть: " +
+                androids.contains(new Phone("Samsung", "pink")));
+
+        System.out.println("Gold Android есть: " +
+                androids.contains(new Phone("Samsung", "gold")));
+
+        System.out.println("Pink Apple есть: " +
+                apples.contains(new Phone("Apple", "pink")));
+
+        System.out.println("Gold Apple есть: " +
+                apples.contains(new Phone("Apple", "gold")));
+
     }
 }
