14e619e2a3891ecce51d184544ef81cfeab1b25a
Michael <michael@ukr.com>
2026-02-24 16:56:14 +0300

    Home 4 and 3

---
+++ src/main/java/org/prog/session5/CarService.java
@@ -0,0 +1,23 @@
+package org.prog.session5;
+
+public class CarService {
+
+    public void paintCar(Car car, String color) {
+        System.out.println("Received car to paint with color: " + car.color);
+        System.out.println("New color " + color);
+        car.color = color;
+        car.goTo( "Owner's home", 45);
+
+    }
+
+    public void addPayment(int amount) {
+        amount = amount + 10;
+        System.out.println("Charge user with " + amount);
+    }
+
+    public void setCarOwner(Car car, String ownerName) {
+        car.owner = ownerName;
+        System.out.println("New owner for car " + car.plateNumber + " is " + ownerName);
+    }
+
+}
