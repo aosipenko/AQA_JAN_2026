a11b55a7c187e716cce3a5ad23fd9092d2297c0e
Sebastian Pereyra <pereyras1976@gmail.com>
2026-03-10 22:03:02 +0200

    Homework15

--- src/test/java/org/prog/session15/WebTests.java
+++ src/test/java/org/prog/session15/WebTests.java
@@ -1,132 +1,115 @@
 package org.prog.session15;
 
 import org.openqa.selenium.By;
-import org.openqa.selenium.JavascriptExecutor;
+import org.openqa.selenium.Keys;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.interactions.Actions;
+
 import org.openqa.selenium.support.ui.ExpectedConditions;
 import org.openqa.selenium.support.ui.WebDriverWait;
+
+import org.testng.Assert;
 import org.testng.annotations.AfterSuite;
 import org.testng.annotations.BeforeSuite;
 import org.testng.annotations.Test;
 
 import java.time.Duration;
-
-//TODO: load allo.ua (done)
-//TODO: search for iphone (done)
-//TODO: find prices for first 3 items
-//TODO: assert price not null
-
-//TODO: * - confirm goods it not null for first 3 items
-//TODO: * - if doing this, use maximize (see below)
+import java.util.List;
 
 public class WebTests {
 
     private WebDriver driver;
 
     @BeforeSuite
-    public void setUp() {
+    public void setup() {
         driver = new ChromeDriver();
         driver.manage().window().maximize();
     }
 
     @AfterSuite
-    public void tearDown() {
+    public void close() {
         driver.quit();
     }
 
     @Test
-    public void actionsTest() {
-        driver.get("https://www.w3schools.com/cssref/tryit.php?filename=trycss_sel_hover");
+    public void alloTest() {
 
-//        JavascriptExecutor jsExecutor = (JavascriptExecutor) driver;
-//        jsExecutor.executeScript("alert('Hello Selenium!')");
+        driver.get("https://allo.ua/");
 
-        WebDriverWait webDriverWait = new WebDriverWait(driver, Duration.ofSeconds(10));
-        WebElement cookiesFrame =
-                webDriverWait.until(ExpectedConditions.presenceOfElementLocated(By.id("fast-cmp-iframe")));
-        driver.switchTo().frame(cookiesFrame);
-        //accept cookies
-        webDriverWait.until(ExpectedConditions.presenceOfElementLocated(By.xpath("//button[text()='Accept']")))
-                .click();
-        driver.switchTo().defaultContent();
+        WebDriverWait wait = new WebDriverWait(driver, Duration.ofSeconds(10));
 
-        WebElement iFrame = webDriverWait.until(
-                ExpectedConditions.presenceOfElementLocated(By.id("iframeResult_0")));
-        driver.switchTo().frame(iFrame);
+        WebElement search = wait.until(
+                ExpectedConditions.visibilityOfElementLocated(By.id("search-form__input"))
+        );
 
-        Actions linkActions = new Actions(driver);
-        WebElement link1 = webDriverWait.until(
-                ExpectedConditions.visibilityOfElementLocated(By.linkText("w3schools.com")));
-        WebElement link2 = webDriverWait.until(
-                ExpectedConditions.visibilityOfElementLocated(By.linkText("wikipedia.org")));
-        linkActions.moveToElement(link1);
-        linkActions.pause(Duration.ofSeconds(5));
-        linkActions.moveToElement(link2);
-        linkActions.pause(Duration.ofSeconds(5));
-        linkActions.perform();
-        System.out.println("done!");
+        search.sendKeys("iphone");
+        search.sendKeys(Keys.ENTER);
 
+        List<WebElement> products = wait.until(
+                ExpectedConditions.visibilityOfAllElementsLocatedBy(By.cssSelector(".product-card"))
+        );
+
+        for (int i = 0; i < 3; i++) {
+
+            WebElement product = products.get(i);
+
+            WebElement title = product.findElement(By.cssSelector(".product-card__title"));
+            WebElement price = product.findElement(By.cssSelector(".sum"));
+
+            System.out.println("Product " + (i + 1) + ": " + title.getText());
+            System.out.println(price.getText());
+
+            Assert.assertNotNull(price.getText());
+        }
     }
 
     @Test
     public void iFrameTest() {
-        driver.get("https://www.w3schools.com/tags/tryit.asp?filename=tryhtml_button_test");
-        WebDriverWait webDriverWait = new WebDriverWait(driver, Duration.ofSeconds(10));
-        WebElement cookiesFrame =
-                webDriverWait.until(ExpectedConditions.presenceOfElementLocated(By.id("fast-cmp-iframe")));
-        driver.switchTo().frame(cookiesFrame);
-        //accept cookies
-        webDriverWait.until(ExpectedConditions.presenceOfElementLocated(By.xpath("//button[text()='Accept']")))
-                .click();
+
+        driver.get("https://www.w3schools.com/html/tryit.asp?filename=tryhtml_iframe");
+
+        WebDriverWait wait = new WebDriverWait(driver, Duration.ofSeconds(10));
+
+        driver.switchTo().frame("iframeResult");
+
+        driver.switchTo().frame(0);
+
+        WebElement header = wait.until(
+                ExpectedConditions.visibilityOfElementLocated(By.tagName("h1"))
+        );
+
+        Assert.assertNotNull(header);
 
         driver.switchTo().defaultContent();
-        //switch to iFrame
-        WebElement iFrame = driver.findElement(By.id("iframeResult"));
-        driver.switchTo().frame(iFrame);
-
-        WebElement element =
-                webDriverWait.until(
-                        ExpectedConditions.visibilityOfElementLocated(
-                                By.xpath("//button[text()='Click Me!']")));
-        System.out.println(element.isDisplayed());
     }
 
     @Test
     public void myWebTest() {
-        driver.get("https://www.cloudflare.com/");
 
-        WebDriverWait webDriverWait = new WebDriverWait(driver, Duration.ofSeconds(5L));
+        driver.get("https://www.w3schools.com/cssref/tryit.php?filename=trycss_sel_hover");
 
-        webDriverWait.until(
-                ExpectedConditions.presenceOfElementLocated(By.id("onetrust-accept-btn-handler")));
+        WebDriverWait wait = new WebDriverWait(driver, Duration.ofSeconds(10));
 
-//        WebElement acceptCookies =webDriverWait.until(
-//                ExpectedConditions.elementToBeClickable(By.id("onetrust-accept-btn-handler")));
-//        acceptCookies.click();
+        driver.switchTo().frame(0);
 
-        webDriverWait
-                .until(
-                        ExpectedConditions.elementToBeClickable(By.id("onetrust-accept-btn-handler")))
-                .click();
+        Actions actions = new Actions(driver);
 
-        WebElement sideMenu = driver.findElement(By.xpath(
-                "//button[@aria-label='Toggle menu']"));
-        sideMenu.click();
+        WebElement link1 = wait.until(
+                ExpectedConditions.visibilityOfElementLocated(By.linkText("w3schools.com"))
+        );
 
-        WebElement loginBtn = driver.findElement(By.linkText("Log in"));
-        loginBtn.click();
+        WebElement link2 = wait.until(
+                ExpectedConditions.visibilityOfElementLocated(By.linkText("wikipedia.org"))
+        );
 
-//        WebElement emailInput = driver.findElement(By.id("email"));
-//        emailInput.click();
-//        emailInput.sendKeys("test@test.com");
-//
-//        WebElement passwordInput = driver.findElement(By.id("password"));
-//        passwordInput.click();
-//        passwordInput.sendKeys("test123");
+        actions.moveToElement(link1)
+                .pause(Duration.ofSeconds(2))
+                .moveToElement(link2)
+                .pause(Duration.ofSeconds(2))
+                .perform();
 
-        System.out.println("done!");
+        driver.switchTo().defaultContent();
     }
 }
