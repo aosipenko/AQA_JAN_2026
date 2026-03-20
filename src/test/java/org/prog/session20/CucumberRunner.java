82ab79c0acae01cce1218db538a77fc947368069
aosipenko <aoskiev@gmail.com>
2026-03-20 18:38:15 +0100

    Session 20: Allure Reports broken tests!

--- src/test/java/org/prog/session20/CucumberRunner.java
+++ src/test/java/org/prog/session20/CucumberRunner.java
@@ -2,10 +2,15 @@ package org.prog.session20;
 
 import io.cucumber.testng.AbstractTestNGCucumberTests;
 import io.cucumber.testng.CucumberOptions;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.prog.session20.steps.DBSteps;
+import org.prog.session20.steps.GoogleSteps;
 import org.testng.annotations.AfterSuite;
 import org.testng.annotations.BeforeSuite;
-import org.testng.annotations.DataProvider;
 
+import java.sql.Connection;
+import java.sql.DriverManager;
 import java.sql.SQLException;
 
 @CucumberOptions(
@@ -26,31 +31,31 @@ public class CucumberRunner extends AbstractTestNGCucumberTests {
 //        return super.scenarios();
 //    }
 
-//    private Connection connection;
-//    private WebDriver driver;
+    private Connection connection;
+    private WebDriver driver;
 
     @BeforeSuite
     public void beforeSuite() throws SQLException {
-//        connection = DriverManager.getConnection(
-//                "jdbc:mysql://localhost:3306/db",
-//                "root",
-//                "password"
-//        );
-//        DBSteps.connection = connection;
-//
-//        driver = new ChromeDriver();
-//        GoogleSteps.driver = driver;
+        connection = DriverManager.getConnection(
+                "jdbc:mysql://localhost:3306/db",
+                "root",
+                "password"
+        );
+        DBSteps.connection = connection;
+
+        driver = new ChromeDriver();
+        GoogleSteps.driver = driver;
     }
 
     @AfterSuite
     public void afterSuite() throws SQLException {
-//        try {
-//            connection.close();
-//        } catch (Exception e) {
-//        }
-//        try {
-//            driver.quit();
-//        } catch (Exception e) {
-//        }
+        try {
+            connection.close();
+        } catch (Exception e) {
+        }
+        try {
+            driver.quit();
+        } catch (Exception e) {
+        }
     }
 }
